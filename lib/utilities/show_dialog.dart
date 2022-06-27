f18ca3f634fe92dc703a398d2a589c918463de00
Wongoose <wong.zhengxiang@gmail.com>
2022-06-27 15:08:16 +0800

    Created VRegCancelDialog

--- lib/utilities/show_dialog.dart
+++ lib/utilities/show_dialog.dart
@@ -2,9 +2,16 @@ import "package:get/get.dart";
 import "package:the_hill_residence/screens/visitor_registration/widgets/vreg_confirm_visitor_dialog.dart";
 import "package:the_hill_residence/shared/my_confirm_dialog.dart";
 
+Future showVRegCancelDialog() async {
+  return Get.dialog(MyConfirmDialog(
+      title: "Discard registration?",
+      body: "Registration data will be lost if you proceed.",
+      actionText: "Discard"));
+}
+
 Future showLogoutDialog() async {
   return Get.dialog(MyConfirmDialog(
-      title: "Confirm logout",
+      title: "Confirm logout?",
       body: "Are you sure to logout of this account?",
       actionText: "Logout"));
 }
