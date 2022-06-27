8373878a22c33902e52ef87a7053cf827a948706
Wongoose <wong.zhengxiang@gmail.com>
2022-06-27 14:42:25 +0800

    Updated vreg_contact

--- lib/screens/visitor_registration/pages/vreg_contact.dart
+++ lib/screens/visitor_registration/pages/vreg_contact.dart
@@ -16,80 +16,53 @@ class VRegContact extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
-          shrinkWrap: true,
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              fillOverscroll: true,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(22, 32, 22, 22),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    MyPageAppBar(
-                      title: "Visitor registration",
-                      appBarType: MyAppBarType.xmark,
-                    ),
-                    SizedBox(height: 40),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    VRegCenterImageText(
-                      imagePath: "assets/images/visitor-info.png",
-                      title: "Visitor info",
-                      description: "How do we identify your visitor?",
-                    ),
-                    SizedBox(height: 30),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Obx(
-                        () => VRegTextField(
-                          value: vregController.visitorPhone.string,
-                          hintText: "Visitor's phone number",
-                          icon: FontAwesomeIcons.phoneAlt,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Obx(
-                        () => VRegTextField(
-                          value: vregController.visitorName.string,
-                          hintText: "Visitor's name",
-                          icon: FontAwesomeIcons.userAlt,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () => vregController.selectNewContact(),
-                      child: Text(
-                        "Select from my contacts",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          decoration: TextDecoration.underline,
-                          color: Theme.of(context).accentColor,
-                        ),
-                      ),
-                    ),
-
-                    Expanded(
-                      child: Container(),
-                    ),
-                    SizedBox(height: 100, width: 50),
-                    // Row - Next button
-                  ],
+        body: CustomScrollView(shrinkWrap: true, slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            fillOverscroll: true,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(22, 32, 22, 22),
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                MyPageAppBar(
+                    title: "Visitor registration",
+                    appBarType: MyAppBarType.xmark),
+                SizedBox(height: 40),
+                Expanded(child: Container()),
+                VRegCenterImageText(
+                    imagePath: "assets/images/visitor-info.png",
+                    title: "Visitor info",
+                    description: "How do we identify your visitor?"),
+                SizedBox(height: 30),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Obx(() => VRegTextField(
+                      value: vregController.visitorPhone.string,
+                      hintText: "Visitor's phone number",
+                      icon: FontAwesomeIcons.phoneAlt)),
                 ),
-              ),
+                SizedBox(height: 10),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Obx(() => VRegTextField(
+                        value: vregController.visitorName.string,
+                        hintText: "Visitor's name",
+                        icon: FontAwesomeIcons.userAlt))),
+                SizedBox(height: 20),
+                GestureDetector(
+                    onTap: () => vregController.selectNewContact(),
+                    child: Text("Select from my contacts",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).accentColor))),
+                Expanded(child: Container()),
+                SizedBox(height: 100, width: 50),
+              ]),
             ),
-          ],
-        ),
-        floatingActionButton: MyRegFAB(
-          onPressed: navigateToVRegDate,
-        ),
+          ),
+        ]),
+        floatingActionButton: MyRegFAB(onPressed: navigateToVRegDate),
         floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
       ),
     );
