cb937cde36af51090117c64130c909b43a80fc15
Wongoose <wong.zhengxiang@gmail.com>
2022-07-05 08:26:55 +0800

    Added switch

--- lib/screens/admin/widgets/manage_unit_dialog.dart
+++ lib/screens/admin/widgets/manage_unit_dialog.dart
@@ -76,6 +76,14 @@ class ManageUnitDialog extends StatelessWidget {
                 ]),
               ])),
           SizedBox(height: 25),
+          Container(
+              margin: EdgeInsets.all(0),
+              padding: EdgeInsets.all(0),
+              child: Switch(
+                  activeColor: Theme.of(context).accentColor,
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  value: true,
+                  onChanged: (value) {})),
           Divider(height: 0),
           IntrinsicHeight(
               child: Row(mainAxisSize: MainAxisSize.max, children: [
