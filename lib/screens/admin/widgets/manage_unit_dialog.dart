a151539561df73d17a34989e44706e01350c5cc1
Wongoose <wong.zhengxiang@gmail.com>
2022-07-05 08:47:09 +0800

    Changed action buttons

--- lib/screens/admin/widgets/manage_unit_dialog.dart
+++ lib/screens/admin/widgets/manage_unit_dialog.dart
@@ -83,39 +83,14 @@ class ManageUnitDialog extends StatelessWidget {
                   ),
                 ]),
               ])),
-          SizedBox(height: 25),
+          SizedBox(height: 10),
           Divider(height: 0),
-          IntrinsicHeight(
-              child: Row(mainAxisSize: MainAxisSize.max, children: [
-            Expanded(
-                child: Container(
-                    margin: EdgeInsets.all(0),
-                    width: MediaQuery.of(context).size.width,
-                    height: 60,
-                    color: Colors.transparent,
-                    child: TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: Icon(Icons.phone, color: Theme.of(context).accentColor)))),
-            VerticalDivider(thickness: 0, width: 0),
-            Expanded(
-                child: Container(
-                    margin: EdgeInsets.all(0),
-                    width: MediaQuery.of(context).size.width,
-                    height: 60,
-                    color: Colors.transparent,
-                    child: TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: Icon(Icons.home, color: Theme.of(context).accentColor)))),
-            VerticalDivider(thickness: 0, width: 0),
-            Expanded(
-                child: Container(
-                    margin: EdgeInsets.all(0),
-                    width: MediaQuery.of(context).size.width,
-                    height: 60,
-                    color: Colors.transparent,
-                    child: TextButton(
-                        onPressed: () => Navigator.pop(context), child: Icon(Icons.delete, color: Colors.red)))),
-          ])),
+          Container(
+              margin: EdgeInsets.all(0),
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              color: Colors.transparent,
+              child: TextButton(onPressed: () => Get.back(), child: MyText("Cancel", color: themeService.textColor54))),
         ]));
   }
 }
