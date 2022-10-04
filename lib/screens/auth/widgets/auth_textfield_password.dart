1ea67540df79ac494f5ebb1eb9af03ce14eb180c
Wongoose <wong.zhengxiang@gmail.com>
2022-10-04 22:31:01 +0800

    Added obscure password state

--- lib/screens/auth/widgets/auth_textfield_password.dart
+++ lib/screens/auth/widgets/auth_textfield_password.dart
@@ -1,28 +1,36 @@
 import "package:flutter/material.dart";
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import "package:get/get.dart";
 import "package:the_hill_residence/controllers/theme_service_controller.dart";
 import "package:the_hill_residence/shared/my_text_input_decoration.dart";
 
-class AuthTextFieldPassword extends StatelessWidget {
-  final MyThemeServiceController themeService =
-      Get.put(MyThemeServiceController());
+class AuthTextFieldPassword extends StatefulWidget {
   final TextEditingController? passwordController;
   final bool showPrefixIcon;
 
-  AuthTextFieldPassword({
+  const AuthTextFieldPassword({
     this.passwordController,
     this.showPrefixIcon = true,
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AuthTextFieldPassword> createState() => _AuthTextFieldPasswordState();
+}
+
+class _AuthTextFieldPasswordState extends State<AuthTextFieldPassword> {
+  final MyThemeServiceController themeService = Get.put(MyThemeServiceController());
+  bool visiblePassword = false;
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: passwordController,
+      controller: widget.passwordController,
       scrollPadding: EdgeInsets.fromLTRB(0, 0, 0, 80),
       style: TextStyle(
         color: themeService.textColor,
       ),
+      obscureText: !visiblePassword,
       decoration: textInputDecoration.copyWith(
         contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
         hintText: "Password",
@@ -30,30 +38,23 @@ class AuthTextFieldPassword extends StatelessWidget {
           color: themeService.textColor54,
         ),
         labelText: "Password",
-        floatingLabelStyle:
-            TextStyle(color: Theme.of(context).primaryColor, fontSize: 18),
-        prefixIcon: showPrefixIcon
+        floatingLabelStyle: TextStyle(color: Theme.of(context).primaryColor, fontSize: 18),
+        prefixIcon: widget.showPrefixIcon
             ? Icon(
                 Icons.lock_outline_rounded,
                 color: themeService.textColor54,
               )
             : null,
-
-        // suffixIcon: GestureDetector(
-        //   onTap: () {
-        //     setState(() =>
-        //         visiblePassword = !visiblePassword);
-        //   },
-        //   child: Icon(
-        //     visiblePassword
-        //         ? FontAwesomeIcons.eye
-        //         : FontAwesomeIcons.eyeSlash,
-        //     size: 18,
-        //     color: visiblePassword
-        //         ? Colors.black
-        //         : Colors.black54,
-        //   ),
-        // ),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() => visiblePassword = !visiblePassword);
+          },
+          child: Icon(
+            visiblePassword ? FontAwesomeIcons.eye : FontAwesomeIcons.eyeSlash,
+            size: 18,
+            color: visiblePassword ? Theme.of(context).primaryColor : Colors.black54,
+          ),
+        ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(10)),
           borderSide: BorderSide(
